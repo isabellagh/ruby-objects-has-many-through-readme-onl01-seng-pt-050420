36c9b7a67f1aafd84c4a3ae3b8c88d843b02f51a
isabella g henriques <isabellagh88@gmail.com>
2020-06-05 23:47:38 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,26 @@
 class Customer
   
+  attr_accessor :name, :age, :total
+  
+  def initialize(name, age)
+    @name = name
+    @age = age 
+    @meal = meal
+    @@all << self
+    
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def new_meal(waiter, total, tip=0)
+    Waiter.new(waiter, self, total, tip)
+    
+  end 
+  
+  def meal 
+    
+  end 
+  
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,19 @@
 class Waiter
 
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self 
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def new_meal(customer, total, tip=0)
+    Meal.new(self, customer, total,tip)
+  end 
+  
+  def meals
+  
 end
